6c1da2d94b8e048642f96cdd0da992ec0df48b07
Alex Wuillaume <wuillaumea@gmail.com>
2023-01-30 19:37:01 -0500

    chapter 1

---
+++ hello/tests/cli.rs
@@ -0,0 +1,31 @@
+use assert_cmd::Command;
+//use std::process::Command;
+
+#[test]
+fn works() {
+    assert!(true);
+}
+
+#[test]
+fn runs() {
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn verifies_output() {
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success().stdout("Hello, world!\n");
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
